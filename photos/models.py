90c6d30027d1c7af9421b6063910094480b8c59b
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-05-29 21:31:04 +0300

    add model classes

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Photos(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+    
+
+class Location(models.Model):
+    name = models.CharField(max_length=40)
+    
