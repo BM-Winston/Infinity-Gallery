108d2bb5fc7ade55a81fc44e9de3987fed57d800
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-05-29 23:55:45 +0300

    Add tests

--- photos/models.py
+++ photos/models.py
@@ -8,6 +8,15 @@ class Photos(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
 
 
+    def __str__(self):
+        return self.name
+    
+    def save_photo(self):
+        self.save()
+    
+ 
+
+
 class Category(models.Model):
     name = models.CharField(max_length=40)
 
--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Photos, Category, Location
 
-# Create your tests here.
+class  PhotosTestCase(TestCase):
+    # Set Up method
+    def setUp(self):
+        self.maldives=Photos(name='Find out why Malldives is popular', description='pristine beaches and sprawling greenery', category=Category(name='islands'), location=Location(name='Indian Ocean'))
+
+        
+
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.maldives,Photos))
+
+
+    
